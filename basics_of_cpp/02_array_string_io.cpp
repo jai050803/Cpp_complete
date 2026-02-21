c072f59665d0dd892418184149391fa0948e9379
jai050803 <sjai5803@gmail.com>
2026-02-21 18:48:16 +0530

    number system conversion method aaded

--- basics_of_cpp/02_array_string_io.cpp
+++ basics_of_cpp/02_array_string_io.cpp
@@ -1,11 +1,12 @@
 // Program 2: Input and Output for Array and Strings
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
     int n;
     cout << "Enter array size: ";
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     cout << "Enter array elements: ";
     for(int i=0; i<n; i++) cin >> arr[i];
     cout << "Array: ";
--- basics_of_cpp/05_number_system_conversion.cpp
+++ basics_of_cpp/05_number_system_conversion.cpp
@@ -1,6 +1,20 @@
 // Program 5: Number System Conversion (Decimal to Binary)
 #include <iostream>
+#include <bitset>
+#include <string>
 using namespace std;
+
+string func(int n){
+    if(n == 0) return "0";
+    string binary = "";
+
+    while(n > 0){
+        binary = char((n % 2) + '0') + binary;
+        n = n/2;
+    }
+    return binary;
+}
+
 int main() {
     int num;
     cout << "Enter decimal number: ";
@@ -8,5 +22,12 @@ int main() {
     cout << "Binary: ";
     for(int i=31; i>=0; i--) cout << ((num>>i)&1);
     cout << "\n";
+    
+    //using bitset method (cleaner and modern way)
+    cout << "binary : " << bitset<32>(num) << endl;
+
+    //using the function created  - without 32 bits leading zeroes
+    cout << func(num) << endl;
     return 0;
 }
+
