99761f2077d78dc27db6c1fb25843d22febac9b7
jai050803 <sjai5803@gmail.com>
2026-02-21 23:05:05 +0530

    array pointers

--- pointers in cpp/01_pointer_basics.cpp
+++ pointers in cpp/01_pointer_basics.cpp
@@ -9,5 +9,16 @@ int main() {
     cout << "Address of a: " << &a << endl;
     cout << "Pointer p holds address: " << p << endl;
     cout << "Value pointed by p: " << *p << endl;
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,1};
+    double *q;
+    p = arr;
+
+    cout << p << endl;
+    cout << *p << endl;
+    cout << *(arr + 6) << endl;
+    cout << arr[6] << endl;
+    cout << arr << endl;
+    cout << *arr << endl; //for array, pointer stores the first eleemnt for array name
     return 0;
 }
