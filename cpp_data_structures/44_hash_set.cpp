b4c27bfdccf148416ff8856ac33c7a6e48644f0c
jai050803 <sjai5803@gmail.com>
2026-02-22 16:03:07 +0530

    hash set functions

--- cpp_data_structures/44_hash_set.cpp
+++ cpp_data_structures/44_hash_set.cpp
@@ -5,10 +5,39 @@ using namespace std;
 
 int main() {
     unordered_set<int> uset;
-    uset.insert(1); uset.insert(2); uset.insert(3);
-    cout << "Hash Set: ";
+
+    // INSERT
+    uset.insert(1);
+    uset.insert(2);
+    uset.insert(3);
+    uset.insert(2); // duplicate (ignored)
+
+    cout << "Initial Hash Set: ";
     for (int x : uset) cout << x << " ";
     cout << endl;
-    cout << "Find 2: " << (uset.count(2) ? "Yes" : "No") << endl;
+
+    // SIZE
+    cout << "Size of Hash Set: " << uset.size() << endl;
+
+    // COUNT (returns 1 if exists, 0 if not)
+    cout << "Count of 2: " << uset.count(2) << endl;
+    cout << "Count of 5: " << uset.count(5) << endl;
+
+    // FIND (returns iterator)
+    if (uset.find(3) != uset.end())
+        cout << "Element 3 found using find()" << endl;
+    else
+        cout << "Element 3 not found" << endl;
+
+    // ERASE
+    uset.erase(2);  // removes element 2
+
+    cout << "Hash Set after erasing 2: ";
+    for (int x : uset) cout << x << " ";
+    cout << endl;
+
+    // Check again after erase
+    cout << "Count of 2 after erase: " << uset.count(2) << endl;
+
     return 0;
 }
